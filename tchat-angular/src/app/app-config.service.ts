66cd978dd80292dac0beabd7eaa396ff824fa64c
Unknown <terence.allart@gmail.com>
2018-09-19 16:27:44 +0200

    update s

--- tchat-angular/src/app/app-config.service.ts
+++ tchat-angular/src/app/app-config.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppConfigService {
 
-  apiUrl: string = 'http://192.168.0.116:8080/api/salon';
+  apiUrl: string = 'http://192.168.0.116:8080/api/';
 
 }
