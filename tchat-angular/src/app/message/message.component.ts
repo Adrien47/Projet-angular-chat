dbf8fe62619893c6993d0cd770fe4b8e728d2a3b
Adrien <adrien.lavelle@hotmail.fr>
2018-09-19 16:59:19 +0200

    commentaire message

--- tchat-angular/src/app/message/message.component.ts
+++ tchat-angular/src/app/message/message.component.ts
@@ -1,45 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {MessageService} from './message.service';
-import {Message} from './message';
-
-@Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
-})
-export class MessageComponent implements OnInit {
-
-  private formMessage: Message = null;
-
-  constructor(private messageService: MessageService) {
-  }
-
-  ngOnInit() {
-  }
-
-  public list() {
-    return this.messageService.findAll();
-  }
-
-  public add() {
-    this.formMessage = new Message();
-  }
-
-  public edit(id: number) {
-    this.formMessage = this.messageService.findById(id);
-  }
-
-  public save() {
-    this.messageService.save(this.formMessage);
-    this.formMessage = null;
-  }
-
-  public delete(id: number) {
-    this.messageService.delete(this.messageService.findById(id));
-  }
-
-  public cancel(id: number) {
-      this.formMessage = null;
-    }
-
-}
+// import {Component, OnInit} from '@angular/core';
+// import {MessageService} from './message.service';
+// import {Message} from './message';
+//
+// @Component({
+//   selector: 'app-message',
+//   templateUrl: './message.component.html',
+//   styleUrls: ['./message.component.css']
+// })
+// export class MessageComponent implements OnInit {
+//
+//   private formMessage: Message = null;
+//
+//   constructor(private messageService: MessageService) {
+//   }
+//
+//   ngOnInit() {
+//   }
+//
+//   public list() {
+//     return this.messageService.findAll();
+//   }
+//
+//   public add() {
+//     this.formMessage = new Message();
+//   }
+//
+//   public edit(id: number) {
+//     this.formMessage = this.messageService.findById(id);
+//   }
+//
+//   public save() {
+//     this.messageService.save(this.formMessage);
+//     this.formMessage = null;
+//   }
+//
+//   public delete(id: number) {
+//     this.messageService.delete(this.messageService.findById(id));
+//   }
+//
+//   public cancel(id: number) {
+//       this.formMessage = null;
+//     }
+//
+// }
--- tchat-angular/src/app/message/message.service.ts
+++ tchat-angular/src/app/message/message.service.ts
@@ -1,83 +1,83 @@
-import {Injectable} from '@angular/core';
-import {Message} from './message';
-
-import {Http} from '@angular/http';
-import {AppConfigService} from '../app-config.service';
-
-
-
-@Injectable()
-export class MessageService {
-  messages: Array<Message> = new Array<Message>();
-  apiUrl = '';
-
-  constructor(private http: Http, private appConfig: AppConfigService) {
-    this.apiUrl = appConfig.apiUrl + '/message/';
-
-    this.http
-      .get(this.apiUrl)
-      .subscribe(resp => this.messages = resp.json());
-
-
-  }
-
-  public findAll() {
-    return this.messages;
-  }
-
-  public findById(id: number, http?: boolean): any {
-    if (http) {
-      return this.http
-        .get(this.apiUrl + id);
-    }
-
-    for (const message of this.messages) {
-      if (message.id === id) {
-        return message;
-      }
-    }
-
-    return null;
-  }
-
-  public save(message: Message) {
-    if (message) {
-      if (!message.id) {
-
-        if (this.messages.length > 0) {
-          message.id = this.messages[this.messages.length - 1].id + 1;
-        } else {
-          message.id = 1;
-        }
-
-
-        this.http
-          .post(this.apiUrl, message)
-          .subscribe(
-            resp => this.messages.push(message),
-            err => console.log(err)
-          );
-
-
-      } else {
-        this.http
-          .put(this.apiUrl + message.id, message)
-          .subscribe(
-            resp => null,
-            err => console.log(err)
-          );
-      }
-    }
-  }
-
-  public delete(message: Message) {
-    const pos: number = this.messages.indexOf(message);
-
-    this.http
-      .delete(this.apiUrl + message.id)
-      .subscribe(
-        resp => this.messages.splice(pos, 1),
-        err => console.log(err)
-      );
-  }
-}
+// import {Injectable} from '@angular/core';
+// import {Message} from './message';
+//
+// import {Http} from '@angular/http';
+// import {AppConfigService} from '../app-config.service';
+//
+//
+//
+// @Injectable()
+// export class MessageService {
+//   messages: Array<Message> = new Array<Message>();
+//   apiUrl = '';
+//
+//   constructor(private http: Http, private appConfig: AppConfigService) {
+//     this.apiUrl = appConfig.apiUrl + '/message/';
+//
+//     this.http
+//       .get(this.apiUrl)
+//       .subscribe(resp => this.messages = resp.json());
+//
+//
+//   }
+//
+//   public findAll() {
+//     return this.messages;
+//   }
+//
+//   public findById(id: number, http?: boolean): any {
+//     if (http) {
+//       return this.http
+//         .get(this.apiUrl + id);
+//     }
+//
+//     for (const message of this.messages) {
+//       if (message.id === id) {
+//         return message;
+//       }
+//     }
+//
+//     return null;
+//   }
+//
+//   public save(message: Message) {
+//     if (message) {
+//       if (!message.id) {
+//
+//         if (this.messages.length > 0) {
+//           message.id = this.messages[this.messages.length - 1].id + 1;
+//         } else {
+//           message.id = 1;
+//         }
+//
+//
+//         this.http
+//           .post(this.apiUrl, message)
+//           .subscribe(
+//             resp => this.messages.push(message),
+//             err => console.log(err)
+//           );
+//
+//
+//       } else {
+//         this.http
+//           .put(this.apiUrl + message.id, message)
+//           .subscribe(
+//             resp => null,
+//             err => console.log(err)
+//           );
+//       }
+//     }
+//   }
+//
+//   public delete(message: Message) {
+//     const pos: number = this.messages.indexOf(message);
+//
+//     this.http
+//       .delete(this.apiUrl + message.id)
+//       .subscribe(
+//         resp => this.messages.splice(pos, 1),
+//         err => console.log(err)
+//       );
+//   }
+// }
